536ce88603e9ff4b1a99c2685eb365669f140670
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-02-16 19:34:07 -0200

    chessException and chessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,15 @@
 package application;
 
+import chess.ChessMatch;
+import util.UI;
+
 /*
  * @author Carlos Henrique
  * @github github.com/httpsantos
  */
 public class Program {
-
 	public static void main(String[] args) {
-
+		ChessMatch c = new ChessMatch();
+		UI.printBoard(c.getPieces());
 	}
 }
---
+++ src/boardgame/exception/ChessException.java
@@ -0,0 +1,15 @@
+package boardgame.exception;
+
+/*
+ * @author Carlos Henrique
+ * @github github.com/httpsantos
+ */
+public class ChessException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String msg) {
+		super(msg);
+	}
+
+}
